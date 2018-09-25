95f5db8f4a5dd561b0c5f4860b7263071db571c5
jarno.ana39 <jarno.ana39@gmail.com>
2018-09-25 11:51:18 -0400

    Test JSON controller

---
+++ app/controllers/testjson_controller.rb
@@ -0,0 +1,8 @@
+class TestjsonController < ApplicationController
+  skip_before_action :verify_authenticity_token, :only => [:create]
+  
+  def create
+    puts params.inspect
+    render json: params
+  end
+end
