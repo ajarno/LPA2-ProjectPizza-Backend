931e0a2ce8b5bc11d03c1dd1d31ce25f02ff97bc
Anais <jarno.ana39@gmail.com>
2018-09-30 18:57:09 +0000

    Rooting problem

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
 #   resources :orders, :only => [:index, :create]
 #   get 'orders/create'
-  get 'orders/index'
+#   get 'orders/index'
   post 'orders/create', to: 'orders#create'
-#   get 'orders', to: 'orders#index'
+  get 'orders', to: 'orders#index'
 
   resources :pizzas
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
