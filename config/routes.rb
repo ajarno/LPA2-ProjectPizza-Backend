da89a31647b8bd98ff2018cacf85f680e8122e9c
Anais <jarno.ana39@gmail.com>
2018-09-30 18:47:34 +0000

    Trying to resolve problem with the create

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   resources :orders, :only => [:index, :create]
-  get 'orders/create'
+#   get 'orders/create'
   get 'orders/index'
-#   post 'orders/create', to: 'orders#create'
+  post 'orders/create', to: 'orders#create'
 #   get 'orders', to: 'orders#index'
 
   resources :pizzas
