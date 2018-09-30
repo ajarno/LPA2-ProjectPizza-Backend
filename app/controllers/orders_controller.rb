505305a670ede5267b176a5683e065d33aba47d2
Anais <jarno.ana39@gmail.com>
2018-09-30 18:30:33 +0000

    Create part of the controller + create view modified

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,6 +2,29 @@ class OrdersController < ApplicationController
     skip_before_action :verify_authenticity_token, :only => [:create]
     
   def create
+    p "ordersController"
+    @nom = params[:nomn]
+    @adresse = params[:adress]
+    p @adresse
+    @pizzas = params[:pizzas]
+    newOrder = Order.new(:nomn => @nom, :adress => @adresse)
+    newOrder.save
+    p @pizzas
+    @erros="Erros : [";
+    @pizzas.each do |pizza|
+	    @orderPizza =Pizza.where(name: pizza).first
+	    if @orderPizza.nil?
+	        @erros= @erros+" - There is no pizza with name = "+pizza
+	        p @erros
+	    next
+        end
+        p @orderPizza
+        newOrderline = Orderline.new
+        newOrderline.order=newOrder
+        newOrderline.pizza=@orderPizza
+        newOrderline.save
+    end
+    @erros = @erros + "]"
   end
 
   def index
