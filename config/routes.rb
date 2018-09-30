1dea8efe06f5351d20eb78f280b321898a3f2aa2
jarno.ana39 <jarno.ana39@gmail.com>
2018-09-30 12:12:40 -0400

    Routing modified

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   post 'orders/create', to: 'orders#create'
   get 'orders/index', to: 'orders#index'
-  resources :orders
+  resources :pizzas
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
