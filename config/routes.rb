c97ab32b471271cf0fc0f78797b18fb57be6d376
Anais <jarno.ana39@gmail.com>
2018-09-30 17:30:43 +0000

    Reroot index orders to orders

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 Rails.application.routes.draw do
-  get 'orders/create'
-  get 'orders/index'
   post 'orders/create', to: 'orders#create'
-  get 'orders/index', to: 'orders#index'
+  get 'orders', to: 'orders#index'
   resources :pizzas
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
