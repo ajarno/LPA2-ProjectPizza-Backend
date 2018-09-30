22de4e193b3019781edbd4b07f456066e8fe76ab
Anais <jarno.ana39@gmail.com>
2018-09-30 18:36:16 +0000

    Careless mistakes corrected

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -3,25 +3,25 @@ class OrdersController < ApplicationController
     
   def create
     p "ordersController"
-    @nom = params[:nomn]
+    @nom = params[:name]
     @adresse = params[:adress]
     p @adresse
     @pizzas = params[:pizzas]
-    newOrder = Order.new(:nomn => @nom, :adress => @adresse)
+    newOrder = Order.new(:name => @name, :adress => @adress)
     newOrder.save
     p @pizzas
     @erros="Erros : [";
     @pizzas.each do |pizza|
 	    @orderPizza =Pizza.where(name: pizza).first
 	    if @orderPizza.nil?
-	        @erros= @erros+" - There is no pizza with name = "+pizza
+	        @erros= @erros+" - There is no pizza with name = " + pizza
 	        p @erros
 	    next
         end
         p @orderPizza
         newOrderline = Orderline.new
-        newOrderline.order=newOrder
-        newOrderline.pizza=@orderPizza
+        newOrderline.order = newOrder
+        newOrderline.pizza = @orderPizza
         newOrderline.save
     end
     @erros = @erros + "]"
