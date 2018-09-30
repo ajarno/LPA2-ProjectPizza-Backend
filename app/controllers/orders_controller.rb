ca4abab4d0cbf8f03044a9980348f8190c4c1d81
Anais <jarno.ana39@gmail.com>
2018-09-30 18:17:06 +0000

    Nil problem to resolve

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,7 +1,22 @@
 class OrdersController < ApplicationController
+    skip_before_action :verify_authenticity_token, :only => [:create]
+    
   def create
   end
 
   def index
+    @orders= []
+    @allOrders = Order.all
+    @allOrders.each do |order|
+        pizzas = []
+        @orderlines=Orderline.where(order: order)
+        @orderlines.each do |orderline|
+            pizza = Pizza.find(orderline[:pizza_id])
+            pizzas.push(pizza)
+        end
+      p "orders "
+      @orders.push({order: order ,pizzas: pizzas})
+      p pizzas
+    end
   end
 end
